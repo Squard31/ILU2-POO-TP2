c0d569ee67e509e58d674f29822d9469b539e926
Squard31 <jidfai@gmail.com>
2025-04-09 14:23:35 +0200

    TP2/3 finit

--- src/controleur/ControlAcheterProduit.java
+++ src/controleur/ControlAcheterProduit.java
@@ -5,13 +5,13 @@ import personnages.Gaulois;
 import villagegaulois.Etal;
 
 public class ControlAcheterProduit {
+
 	private Village village;
 	private ControlTrouverEtalVendeur controlTrouverEtalVendeur;
 	private ControlVerifierIdentite controlVerifierIdentite;
 
 	public ControlAcheterProduit(ControlVerifierIdentite controlVerifierIdentite,
-			ControlTrouverEtalVendeur controlTrouverEtalVendeur,
-			Village village) {
+			ControlTrouverEtalVendeur controlTrouverEtalVendeur, Village village) {
 		this.village = village;
 		this.controlVerifierIdentite = controlVerifierIdentite;
 		this.controlTrouverEtalVendeur = controlTrouverEtalVendeur;
@@ -21,16 +21,19 @@ public class ControlAcheterProduit {
 		boolean identite = controlVerifierIdentite.verifierIdentite(nomVendeur);
 		return identite;
 	}
-	
-	public Gaulois[] getVendeurProduit(String produit){
+
+	public Gaulois[] getVendeurProduit(String produit) {
 		Gaulois[] vendeur = village.rechercherVendeursProduit(produit);
 		return vendeur;
 	}
-	
+
 	public int acheterProduit(Gaulois vendeur, int quantite) {
-		Etal etal = village.rechercherEtal(vendeur);
-		
-		int produit =  etal.acheterProduit(quantite);
-		return produit;
+		Etal etal = controlTrouverEtalVendeur.trouverEtalVendeur(vendeur.getNom());
+		if (etal != null) {
+			int produit = etal.acheterProduit(quantite);
+			return produit;
+		} else {
+			return 0;
+		}
 	}
 }
